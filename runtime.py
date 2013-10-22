d69d0edd64c78e2cae780550b318815c5547689c
Peter Doherty <peter_doherty@hms.harvard.edu>
2013-10-21 22:56:31 -0400

    vastly improved argument handling, reduced memory footprint, reduced computational overhead, and other awesomeness

--- runtime.py
+++ runtime.py
@@ -71,10 +71,10 @@ parser.add_argument('--exitzero', '-z',
                 dest='exitzero',
                 help='exclude jobs with non-zero exit codes')
 
-parser.add_argument('--sumusers',
+parser.add_argument('--nosumusers',
                 action="store_true",
-                dest='sumusers',
-                help='if set, a sum total is calculated for the list of users, instead of processing them individually')
+                dest='nosumusers',
+                help='if set, totals are calculated for each user, instead of processing them collectively')
 
 parser.add_argument('--graphs', '-g',
                 type=str,
@@ -115,9 +115,10 @@ if args.infile and not os.path.isfile(args.infile):
     exit(1)
 
 # Queue definitions
-q_contrib   = ['church_int_15m','danuser_int_15m','freedberg_int_15m','i2b2_int_15m','megason_int_15m','merfeld_int_15m','nezafat_int_15m','nowak_int_15m','park_int_15m','sorger_int_15m','sysbio_int_15m','usheva_int_15m','church_int_2h','danuser_int_2h','freedberg_int_2h','i2b2_int_2h','kreiman_int_2h','megason_int_2h','merfeld_int_2h','nezafat_int_2h','nowak_int_2h','park_int_2h','sorger_int_2h','sysbio_int_2h','usheva_int_2h','bpf_int_12h','cbi_int_12h','church_int_12h','danuser_int_7d','danuser_int_12h','freedberg_int_12h','i2b2_int_12h','kreiman_int_12h','megason_int_12h','merfeld_int_12h','nezafat_int_12h','nowak_int_12h','park_int_12h','sorger_int_2d','sysbio_int_2d','usheva_int_12h','bpf_15m','church_15m','danuser_15m','freedberg_15m','i2b2_15m','kreiman_15m','megason_15m','merfeld_15m','nezafat_15m','nowak_15m','park_15m','sorger_15m','sysbio_15m','usheva_15m','church_2h','danuser_2h','freedberg_2h','i2b2_2h','kreiman_2h','megason_2h','merfeld_2h','nezafat_2h','nowak_2h','park_2h','sorger_2h','sysbio_2h','usheva_2h','bpf_12h','cbi_12h','church_12h','danuser_12h','freedberg_12h','i2b2_12h','kreiman_12h','megason_12h','merfeld_12h','nezafat_12h','nowak_12h','park_12h','sorger_12h','sysbio_12h','usheva_12h','church_1d','danuser_1d','freedberg_1d','i2b2_1d','kreiman_1d','megason_1d','merfeld_1d','nezafat_1d','nowak_1d','park_1d','sorger_1d','sysbio_1d','usheva_1d','church_7d','danuser_7d','freedberg_7d','i2b2_7d','megason_7d','merfeld_7d','nezafat_7d','nowak_7d','park_7d','sorger_7d','sysbio_7d','usheva_7d','bpf_unlimited','cbi_unlimited','church_unlimited','danuser_unlimited','freedberg_unlimited','i2b2_unlimited','kreiman_unlimited','megason_unlimited','merfeld_unlimited','nezafat_unlimited','nowak_unlimited','park_unlimited','sorger_unlimited','sorger_par_unlimited','sorger_par_1d','sysbio_unlimited','sysbio_par_1d','usheva_unlimited','rodeo_15m','rodeo_12h','rodeo_unlimited','reich','seidman']
-q_shared    = ['priority','interactive','mpi','mcore','parallel','short','long','mini']
-q_all       = q_contrib+q_shared 
+d_queues   = {
+    'contrib': ['church_int_15m','danuser_int_15m','freedberg_int_15m','i2b2_int_15m','megason_int_15m','merfeld_int_15m','nezafat_int_15m','nowak_int_15m','park_int_15m','sorger_int_15m','sysbio_int_15m','usheva_int_15m','church_int_2h','danuser_int_2h','freedberg_int_2h','i2b2_int_2h','kreiman_int_2h','megason_int_2h','merfeld_int_2h','nezafat_int_2h','nowak_int_2h','park_int_2h','sorger_int_2h','sysbio_int_2h','usheva_int_2h','bpf_int_12h','cbi_int_12h','church_int_12h','danuser_int_7d','danuser_int_12h','freedberg_int_12h','i2b2_int_12h','kreiman_int_12h','megason_int_12h','merfeld_int_12h','nezafat_int_12h','nowak_int_12h','park_int_12h','sorger_int_2d','sysbio_int_2d','usheva_int_12h','bpf_15m','church_15m','danuser_15m','freedberg_15m','i2b2_15m','kreiman_15m','megason_15m','merfeld_15m','nezafat_15m','nowak_15m','park_15m','sorger_15m','sysbio_15m','usheva_15m','church_2h','danuser_2h','freedberg_2h','i2b2_2h','kreiman_2h','megason_2h','merfeld_2h','nezafat_2h','nowak_2h','park_2h','sorger_2h','sysbio_2h','usheva_2h','bpf_12h','cbi_12h','church_12h','danuser_12h','freedberg_12h','i2b2_12h','kreiman_12h','megason_12h','merfeld_12h','nezafat_12h','nowak_12h','park_12h','sorger_12h','sysbio_12h','usheva_12h','church_1d','danuser_1d','freedberg_1d','i2b2_1d','kreiman_1d','megason_1d','merfeld_1d','nezafat_1d','nowak_1d','park_1d','sorger_1d','sysbio_1d','usheva_1d','church_7d','danuser_7d','freedberg_7d','i2b2_7d','megason_7d','merfeld_7d','nezafat_7d','nowak_7d','park_7d','sorger_7d','sysbio_7d','usheva_7d','bpf_unlimited','cbi_unlimited','church_unlimited','danuser_unlimited','freedberg_unlimited','i2b2_unlimited','kreiman_unlimited','megason_unlimited','merfeld_unlimited','nezafat_unlimited','nowak_unlimited','park_unlimited','sorger_unlimited','sorger_par_unlimited','sorger_par_1d','sysbio_unlimited','sysbio_par_1d','usheva_unlimited','rodeo_15m','rodeo_12h','rodeo_unlimited','reich','seidman'],
+    'shared': ['priority','interactive','mpi','mcore','parallel','short','long','mini']
+}
 
 
 # Dictionary that defines the structure of the data file
@@ -139,6 +140,20 @@ d_pos = {
     'unkwn_t':  [14,'int'],
     'dep':      [15,'str']
 }
+
+# Dictionary with the parameters for the various graphs.
+d_figs = {
+    'cpu_usage': [0,'hist','cpu_usage','Number of Jobs','CPU Usage (hours per job)','Histogram of CPU Usage',range(0,720,1)],
+    'runtime': [1,'hist','runtime','Number of Jobs','Wall Clock Time (sec per job)','Histogram of Job Run Times',range(0,2592000,60)],
+    'mem_reserved': [2,'hist','mem_reserved','Number of Jobs','Memory Reserved (GB per core)','Histogram of Memory Reservations',range(0,96,2)],
+    'mem_used': [3,'hist','mem_used','Number of Jobs','Memory Used (GB per job)','Histogram of Memory Usage',range(0,64,1)],
+    'ncpu': [4,'hist','number_cores','Number of Jobs','Number of Cores Reserved','Histogram of Core Reservation',range(1,128,1)],
+    'eff': [5,'hist','efficiency','Number of Jobs','Job Efficiency ((CPU Usage*Cores)/RunTime)','Histogram of Job Efficiency',range(10,500,20)],
+    'memdelta': [6,'hist','mem_delta','Number of Jobs','(Mem. Reserved) - (Mem. Used)','Histogram of Memory Efficiency',range(-8,64,2)],
+    'memscat': [7,'scatter','mem_scat','Mem. Used (MB)','Mem. Reserved (MB)','Scatter Plot of Memory Efficiency']
+    }
+
+
 
 def makeintorzero(v):
     '''Convert a string into an int if possible, or set it to 0'''
@@ -170,9 +185,9 @@ def create_filtered_list(datafile,d_args):
     else:
         unames = []
     if d_args['q'] and d_args['q'] == "contrib":
-        qnames   = q_contrib
+        qnames   = d_queues['contrib']
     elif d_args['q'] and d_args['q'] == "shared":
-        qnames   = q_shared
+        qnames   = d_queues['shared']
     elif d_args['q']:
         qnames = d_args['q'].split(",")
     else:
@@ -218,8 +233,11 @@ def create_filtered_list(datafile,d_args):
 def calc(data_bin):
     gc.disable()
     result      = data_bin
-    d_calc      = {'l_cpu': [], 'l_r':[], 'l_mrsv': [], 'l_mused': [], 'l_ncpu': [], 'l_eff': [], 'l_memdelta': [] }
+#    print result
+#    d_calc      = dict.fromkeys(d_figs.keys())
+    d_calc      = dict((key,[]) for key in d_figs.keys())
     for line in result:
+#        print line
 #        jid     = line[0]
         u       = line[1]
 #        exit    = line[2]
@@ -237,13 +255,14 @@ def calc(data_bin):
 #        ssusp_t = line[13]
 #        unk_t   = line[14]
 #        dep     = line[15]
-        d_calc['l_cpu'].append(c_used/3600.0)
-        d_calc['l_r'].append(run_t)
-        d_calc['l_mrsv'].append(m_rsv*n_cpu)
-        d_calc['l_mused'].append(m_used/1048576.0)
-        d_calc['l_ncpu'].append(n_cpu)
-        d_calc['l_eff'].append(eff)
-        d_calc['l_memdelta'].append(m_rsv-m_used)
+        d_calc['cpu_usage'].append(c_used/3600.0)
+        d_calc['runtime'].append(run_t)
+        d_calc['mem_reserved'].append(m_rsv*n_cpu)
+        d_calc['mem_used'].append(m_used/1048576.0)
+        d_calc['ncpu'].append(n_cpu)
+        d_calc['eff'].append(eff)
+        d_calc['memdelta'].append(m_rsv-m_used)
+        d_calc['memscat'].append([m_rsv,m_used/1048576.0])
     gc.enable()
     return d_calc
 
@@ -283,55 +302,36 @@ def create_dict(l_parsed):
             u_dict[u] = [line]
     return q_dict, u_dict
 
-def new_print_results(d_results,lu_merged):
-    users_total = []
+def new_print_results(user,d_results):
+#    print d_results['cpu_usage']
     if not args.csv:
-        for user in d_results.keys():
-            l_result    = calc(d_results[user])
-            n_jobs      = len(l_result['l_cpu'])
-            c_total     = sum(l_result['l_cpu'])
-            print "users: %s\n%0.1f cpu hours and %d jobs" % (user,c_total,n_jobs)
+        n_jobs      = len(d_results['cpu_usage'])
+        c_total     = sum(d_results['cpu_usage'])
+        print "users: %s\n%0.1f cpu hours and %d jobs" % (user,c_total,n_jobs)
     elif args.csv:
-        print "user,cpu_hours,numjobs"
-        for user in d_results.keys():
-            l_result    = calc(d_results[user])
-            n_jobs      = len(l_result['l_cpu'])
-            c_total     = sum(l_result['l_cpu'])
-            print "%s,%0.1f,%d" % (user,c_total,n_jobs)
-    l_result    = lu_merged
-    n_jobs      = len(l_result['l_cpu'])
-    c_total     = sum(l_result['l_cpu'])
-    print "total,%0.1f,%d" % (c_total,n_jobs)
+        n_jobs      = len(d_results['cpu_usage'])
+        c_total     = sum(d_results['cpu_usage'])
+        print "%s,%0.1f,%d" % (user,c_total,n_jobs)
 
-def make_csv(d_results,lu_merged):
-    for user in d_results.keys():
-        l_result    = calc(d_results[user])
-        for metric in l_result.keys():
-            out_fn      = user+"_"+metric.replace("l_","")+".csv"
-            out_fh      = open(out_fn, 'w')
-#            out_csv     = csv.writer(out_fh, delimiter=',',quoting=csv.QUOTE_ALL)
-            out_csv     = csv.writer(out_fh, delimiter='\n')
-            out_fh.write("%s,%s\n" % (user,metric.replace("l_","")))
-            out_csv.writerow(sorted(l_result[metric]))
-            out_fh.close()
-    for metric in lu_merged.keys():
-        tot_fn      = "total_"+metric.replace("l_","")+".csv"
-        tot_fh      = open(tot_fn, 'w')
-        tot_csv     = csv.writer(tot_fh, delimiter='\n')
-        tot_fh.write("total,%s\n" % (metric.replace("l_","")))
-        tot_csv.writerow(sorted(lu_merged[metric]))
-        tot_fh.close()
+def make_csv(user,d_results):
+    for metric in d_results.keys():
+        out_fn      = user+"_"+metric+".csv"
+        out_fh      = open(out_fn, 'w')
+        out_csv     = csv.writer(out_fh, delimiter='\n')
+        out_fh.write("%s,%s\n" % (user,metric))
+        out_csv.writerow(sorted(d_results[metric]))
+        out_fh.close()
 
 # Functions for creating a histogram
-def draw_hist(l_input,save):
+def draw_hist(l_input,user,l_result,save):
     fignum = l_input[0]
     figname = l_input[2]
-    figfile = '%s.png' % l_input[2]
+    figfile = '%s_%s.png' % (user,l_input[2])
     figylab = l_input[3]
     figxlab = l_input[4]
     figtit = '%s - (source: %s)\nFilters: %s' % (l_input[5],args.infile, filter_names)
-    figdata = l_input[6]
-    figbins = l_input[7]
+    figdata = l_result
+    figbins = l_input[6]
 #    figxticks = l_input[8]
     plt.figure(fignum)
     plt.ylabel(figylab)
@@ -349,19 +349,21 @@ def draw_hist(l_input,save):
     plt.draw()
     if save:
         plt.savefig(figfile, dpi=300)
+        plt.close()
 
 # Functions for creating a bar graph
-def draw_bar(l_input,save):
+def draw_bar(l_input,user,l_result,save):
     fignum  = l_input[0]
-    figfile = '%s.png' % l_input[1]
-    figylab = l_input[2]
-    figxlab = l_input[3]
-    figtit  = '%s - (source: %s)\nFilters: %s' % (l_input[4],args.infile, filter_names)
+    figfile = '%s_%s.png' % (user,l_input[2])
+    figylab = l_input[3]
+    figxlab = l_input[4]
+    figtit  = '%s - (source: %s)\nFilters: %s' % (l_input[5],args.infile, filter_names)
+    figdata = l_result
     figbins = l_input[6]
     if figbins and figbins != "auto":    
-        figdata_y, figdata_x = np.histogram(l_input[5], bins=figbins)
+        figdata_y, figdata_x = np.histogram(figdata, bins=figbins)
     elif not figbins or figbins == "auto":    
-        figdata_y, figdata_x = np.histogram(l_input[5])
+        figdata_y, figdata_x = np.histogram(figdata)
     figdata_x = figdata_x[:-1]
     plt.figure(fignum)
     plt.ylabel(figylab)
@@ -379,16 +381,18 @@ def draw_bar(l_input,save):
 #    plt.bar(figdata_x,figdata_y,width=1.0,align='center')
 #    plt.bar(range(len(figdata_x)),figdata_y,width=1.0,align='center')
     plt.draw()
+    if save:
+        plt.savefig(figfile, dpi=300)
+
 
 # Function for drawing a Scatter plot
-def draw_scatter(l_input,save):
+def draw_scatter(l_input,user,l_result,save):
     fignum  = l_input[0]
-    figfile = '%s.png' % l_input[2]
+    figfile = '%s_%s.png' % (user,l_input[2])
     figylab = l_input[3]
     figxlab = l_input[4]
     figtit  = '%s - (source: %s)\nFilters: %s' % (l_input[5],args.infile, filter_names)
-    figdata_y = l_input[6]
-    figdata_x = l_input[7]
+    figdata_x,figdata_y = zip(*l_result)
     plt.figure(fignum)
     plt.ylabel(figylab)
     plt.xlabel(figxlab)
@@ -403,6 +407,35 @@ def draw_scatter(l_input,save):
     plt.scatter(figdata_x,figdata_y,c='b',marker='.')
     plt.plot([0,100],[0,100])
     plt.draw()
+    if save:
+        plt.savefig(figfile, dpi=300)
+        plt.close()
+
+def make_graphs(user,d_uresults):
+    if args.debug:
+        t_start = time.time()
+    if args.savegraphs and not args.showgraphs:
+        import matplotlib
+        matplotlib.use("agg")
+    global plt
+    import matplotlib.pyplot as plt
+    if args.graphs == "all" or not args.graphs:
+        l_graph = d_figs.keys()
+    else:
+        l_graph = args.graphs.split(",")
+    for graph in l_graph:
+        if d_figs[graph][1] == "scatter":
+            draw_scatter(d_figs[graph],user,d_uresults[graph],args.savegraphs)
+        elif d_figs[graph][1] == "hist":
+            draw_hist(d_figs[graph],user,d_uresults[graph],args.savegraphs)
+        elif d_figs[graph][1] == "bar":
+            draw_bar(d_figs[graph],user,d_uresults[graph],args.savegraphs)
+    if args.showgraphs or args.graphs:
+        plt.show()
+    if args.debug:
+        t_fin = time.time()
+        print "finished making graphs in: %8.2f seconds." % (t_fin-t_start)
+
 
 u_merged    = []
 d_args      = args.__dict__
@@ -431,53 +464,34 @@ if args.debug:
 
 for user in u_dict.keys():
     u_merged.extend(u_dict[user])
-mrg_u_result    = calc(u_merged)
-
-if not args.quiet:
-    new_print_results(u_dict,mrg_u_result)
-if args.csv:
-    make_csv(u_dict,mrg_u_result)
 
 if args.debug:
     t_fin = time.time()
     print "new_print_results() and make_csv() completed in: %8.2f seconds." % (t_fin-t_start)
 
-
-# Dictionary with the parameters for the various graphs.
-d_figs = {
-    'cpu_usage': [0,'hist','cpu_usage','Number of Jobs','CPU Usage (hours per job)','Histogram of CPU Usage',mrg_u_result['l_cpu'],range(0,720,1)],
-    'runtime': [1,'hist','runtime','Number of Jobs','Wall Clock Time (sec per job)','Histogram of Job Run Times',mrg_u_result['l_r'],range(0,2592000,60)],
-    'mem_reserved': [2,'hist','mem_reserved','Number of Jobs','Memory Reserved (GB per core)','Histogram of Memory Reservations',mrg_u_result['l_mrsv'],range(0,96,2)],
-    'mem_used': [3,'hist','mem_used','Number of Jobs','Memory Used (GB per job)','Histogram of Memory Usage',mrg_u_result['l_mused'],range(0,64,1)],
-    'ncpu': [4,'hist','number_cores','Number of Jobs','Number of Cores Reserved','Histogram of Core Reservation',mrg_u_result['l_ncpu'],range(1,128,1)],
-    'eff': [5,'hist','efficiency','Number of Jobs','Job Efficiency ((CPU Usage*Cores)/RunTime)','Histogram of Job Efficiency',mrg_u_result['l_eff'],range(10,500,20)],
-    'memdelta': [6,'hist','mem_delta','Number of Jobs','(Mem. Reserved) - (Mem. Used)','Histogram of Memory Efficiency',mrg_u_result['l_memdelta'],range(-8,64,2)],
-    'memscat': [7,'scatter','mem_scat','Mem. Used (MB)','Mem. Reserved (MB)','Scatter Plot of Memory Efficiency',mrg_u_result['l_mused'],mrg_u_result['l_mrsv']]
-    }
-
-
-if args.showgraphs or args.savegraphs or args.graphs:
-    if args.debug:
-        t_start = time.time()
-    if args.savegraphs and not args.showgraphs:
-        import matplotlib
-        matplotlib.use("agg")
-    import matplotlib.pyplot as plt
-    if args.graphs == "all" or not args.graphs:
-        l_graph = d_figs.keys()
-    else:
-        l_graph = args.graphs.split(",")
-    for graph in l_graph:
-        if d_figs[graph][1] == "scatter":
-            draw_scatter(d_figs[graph],args.savegraphs)
-        elif d_figs[graph][1] == "hist":
-            draw_hist(d_figs[graph],args.savegraphs)
-        elif d_figs[graph][1] == "bar":
-            draw_bar(d_figs[graph],args.savegraphs)
-    if args.showgraphs or args.graphs:
-        plt.show()
-    if args.debug:
-        t_fin = time.time()
-        print "finished making graphs in: %8.2f seconds." % (t_fin-t_start)
-
+if args.nosumusers:
+    d_umerged   = calc(u_merged)
+    if args.csv:
+        print "user,cpu_hours,numjobs"
+    for user in u_dict.keys():
+        d_uresult   = calc(u_dict[user])
+        if not args.csv:
+            new_print_results(user,d_uresult)
+        elif args.csv:
+            new_print_results(user,d_uresult)
+            make_csv(user,d_uresult)
+            make_csv('total',d_umerged)
+        if args.showgraphs or args.savegraphs or args.graphs:
+            make_graphs(user,d_uresult)
+    new_print_results('total',d_umerged)
+elif not args.nosumusers:
+    d_umerged = calc(u_merged)
+    if not args.csv:
+        new_print_results('total',d_umerged)
+    elif args.csv:
+        print "user,cpu_hours,numjobs"
+        new_print_results('total',d_umerged)
+        make_csv('total',d_umerged)
+    if args.showgraphs or args.savegraphs or args.graphs:
+        make_graphs('total',d_umerged)
 
