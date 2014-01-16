f2c649e29718d0647ef52ed8070987256ab60e7d
Peter Doherty <peter_doherty@hms.harvard.edu>
2014-01-16 18:35:04 -0500

    added graphing capability

--- mem_reserved.py
+++ mem_reserved.py
@@ -16,6 +16,9 @@ import sys
 import os
 import csv
 import numpy as np
+import matplotlib
+matplotlib.use("agg")
+import matplotlib.pyplot as plt
 
 mem_bins    = [0,0.1,0.25,0.8,1.05,float("inf")]
 
@@ -54,9 +57,16 @@ parser.add_argument('--nodefault',
                 dest='nodefault',
                 help='don\'t display jobs with default memory reservation')
 
+parser.add_argument('--graphs',
+                action="store_true",
+                dest='graphs',
+                help='save graphs as png files in ./graphs/')
+
+
 args = parser.parse_args()
 
-#print args.infile
+if not os.path.exists("./graphs"):
+    os.makedirs("./graphs")
 
 #l_infiles   = args.infile.split(" ")
 l_infiles   = args.infile
@@ -78,7 +88,9 @@ def read_tsv(l_infiles):
 
 def make_user_dicts(list_of_jobs):
     '''create up to two keys per user with the calcuated usage ratio.  jobs within a reservation, get put into the user.default key'''
+    d_users     = {}
     d_results   = {}
+    d_graphs    = {}
 #    for line in list_of_jobs[1:]:
     for line in list_of_jobs:
         jobid   = int(line[0])
@@ -99,10 +111,22 @@ def make_user_dicts(list_of_jobs):
                 d_results[user].append(jratio)
             elif not d_results.has_key(user):
                 d_results[user] = [jratio]
-    return d_results
+        if d_users.has_key(user) and m_rsv > args.minrsv*1024: 
+            d_users[user]      += 1
+            d_graphs[user+".x"].append(m_rsv/1048576.0)
+            d_graphs[user+".y"].append((m_used/1048576.0)*n_cpu)
+        elif not d_users.has_key(user): 
+            d_users[user]      = 0
+            d_graphs[user+".x"] = [m_rsv/1048576.0]
+            d_graphs[user+".y"] = [(m_used/1048576.0)*n_cpu]
+
+    return d_users, d_results, d_graphs
 
 l_jobs      = read_tsv(l_infiles)
-d_uresults  = make_user_dicts(l_jobs)
+d_udicts    = make_user_dicts(l_jobs)
+d_uusers    = d_udicts[0]
+d_uresults  = d_udicts[1]
+d_ugraphs   = d_udicts[2]
 
 def print_results():
     print "user            -  <10%  - 10-25% - 25-79% - 80-105% - >105%"
@@ -111,4 +135,24 @@ def print_results():
         if sum(u_result[0]) > args.minjobs:
             print "%-15s - %-6d - %-6d - %-6d - %-6d  - %-6d" % (user, u_result[0][0], u_result[0][1], u_result[0][2], u_result[0][3], u_result[0][4])
 
+def make_plot():
+    for user in sorted(d_uusers.keys()):
+        if d_uusers[user] > args.minjobs:
+            x   = d_ugraphs[user+".x"]
+            y   = d_ugraphs[user+".y"]
+            plot_file   = "./graphs/"+user+".png"
+            plt.figure(0)
+            plt.ylabel("Memory used")
+            plt.xlabel("Memory reserved")
+            plt.grid(True)
+            plt.scatter(x,y,c='b',marker='.')
+            plt.plot([0,100],[0,100])
+            plt.xlim(0,max(y))
+            plt.ylim(0,max(y))
+            plt.draw()
+            plt.savefig(plot_file, dpi=300)
+            plt.close()
+
 print_results()
+if args.graphs:
+    make_plot()
